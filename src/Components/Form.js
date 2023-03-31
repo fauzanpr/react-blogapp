305b8aed71c3c85978d56b32b430df5b410eaa4a
fauzanpr <fauzanpr.06@gmail.com>
2023-03-31 09:20:57 +0700

    add styling on form page

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -6,28 +6,34 @@ const Form = () => {
   const [author, setAuthor] = useState("mario");
   return (
     <form action="">
-      <label htmlFor="blog-title">Blog Title</label>
+      <label className="form__title">Blog Title</label>
       <input
         type="text"
         id="blog-title"
         value={title}
         onChange={(e) => setTitle(e.target.value)}
+        className='form__input'
       />
-      <label htmlFor="blog-body">Blog Body</label>
+      <label className="form__title">Blog Body</label>
       <textarea
         id="blog-body"
+        rows={15}
+        cols={30}
         value={body}
         onChange={(e) => setBody(e.target.value)}
+        className='form__input tr'
       ></textarea>
       <select
         name=""
         id=""
         value={author}
         onChange={(e) => setAuthor(e.target.value)}
+        className={'form__input slct'}
       >
         <option value="mario">Mario</option>
         <option value="luigi">Luigi</option>
       </select>
+      <input type="button" value={'submit'} className="btn-form" />
     </form>
   );
 };
